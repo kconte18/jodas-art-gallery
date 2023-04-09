a9af58c0b53f6da69f12b2ab2c53758b36cd20db
kconte18 <kconte810@gmail.com>
2023-04-08 20:56:58 -0500

    Navbar items are listed, header container set to grid

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,6 @@
 import WebsiteTitle from "./website-title/WebsiteTitle";
 import NavBar from "./navbar/NavBar";
+import "./Header.css";
 
 export default function Header(){
     return (
--- src/components/header/navbar/NavBar.tsx
+++ src/components/header/navbar/NavBar.tsx
@@ -1,7 +1,11 @@
+import "./NavBar.css";
+
 export default function NavBar(){
     return(
-        <div className="navbar-container">
-            <p>Navigation Bar</p>
-        </div>
+        <ul className="navbar-container">
+            <li className="navbar-item">Art Gallery</li>
+            <li className="navbar-item">About</li>
+            <li className="navbar-item">Contact Me</li>
+        </ul>
     )
 }
