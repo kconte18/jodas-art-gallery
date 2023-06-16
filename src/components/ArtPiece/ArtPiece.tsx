0eaffa2716688e047b6a854803561fc961dafac8
kconte18 <kconte810@gmail.com>
2023-06-16 16:46:11 -0500

    Added card as wrapper

--- src/components/ArtPiece/ArtPiece.tsx
+++ src/components/ArtPiece/ArtPiece.tsx
@@ -1,12 +1,15 @@
 import styles from "./ArtPiece.module.css";
 import { ArtPieceModel } from "../../models/ArtPieceModel";
+import Card from "../../shared/Card/Card";
 
 export default function ArtPiece(props: {artPiece: ArtPieceModel}) {
 
     return (
-        <div className={styles["art-piece-container"]}>
-            <img className={styles["main-picture"]} src={props.artPiece.mainPicture} alt="Not Found"/>
-            <p className={styles.artist}>{props.artPiece.artist}</p>
-        </div>
+        <Card>
+            <div className={styles["art-piece-container"]}>
+                <img className={styles["main-picture"]} src={props.artPiece.mainPicture} alt="Not Found"/>
+                <p className={styles.artist}>{props.artPiece.artist}</p>
+            </div>
+        </Card>
     );
 }
--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -3,13 +3,13 @@ import styles from "./Pagination.module.css";
 export default function Pagination() {
     return (
         <ul className={styles["pagination-container"]}>
-            <li className={styles["pagination-container"]}>&lt;&lt;</li>
-            <li className={styles["pagination-container"]}>1</li>
-            <li className={styles["pagination-container"]}>2</li>
-            <li className={styles["pagination-container"]}>3</li>
-            <li className={styles["pagination-container"]}>4</li>
-            <li className={styles["pagination-container"]}>5</li>
-            <li className={styles["pagination-container"]}>&gt;&gt;</li>
+            <li className={styles["pagination-item"]}>&lt;&lt;</li>
+            <li className={styles["pagination-item"]}>1</li>
+            <li className={styles["pagination-item"]}>2</li>
+            <li className={styles["pagination-item"]}>3</li>
+            <li className={styles["pagination-item"]}>4</li>
+            <li className={styles["pagination-item"]}>5</li>
+            <li className={styles["pagination-item"]}>&gt;&gt;</li>
         </ul>
     );
 }
