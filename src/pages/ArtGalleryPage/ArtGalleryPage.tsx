dfb358ce66fbbb5c3110184b760bf751fd643148
kconte18 <kconte810@gmail.com>
2023-06-17 18:23:16 -0500

    Added useEffect to pageNum, setPageNum has been implemented

--- src/pages/ArtGalleryPage/ArtGalleryPage.tsx
+++ src/pages/ArtGalleryPage/ArtGalleryPage.tsx
@@ -1,14 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ArtPiece from "../../components/ArtPiece/ArtPiece";
 import Pagination from "../../components/Pagination/Pagination";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import FakeData from "../../data/FakeData.json";
 import styles from "./ArtGalleryPage.module.css";
 
-const maxPageNum: number = Math.ceil(FakeData.length/6);
+const maxPageNum: number = Math.ceil(FakeData.length / 6);
 
 export default function ArtGalleryPage() {
-    const [shownArtPieces, setShowArtPieces] = useState(FakeData.slice(0,6));
+    const [shownArtPieces, setShowArtPieces] = useState(FakeData.slice(0, 6));
     const [pageNum, setPageNum] = useState(1);
 
     const handleSetShowArtPieces = () => {
@@ -16,26 +16,28 @@ export default function ArtGalleryPage() {
     }
 
     const handleSetPageNum = (event: any) => {
-        let varPageNum: number = 0;
-        if(event.target.textContent == pageNum.toString()) {
+        if (event.target.textContent == pageNum.toString()) {
             console.log("Same Page");
             return;
         }
-        else if(event.target.textContent == ">>") {
+        else if (event.target.textContent == ">>") {
             console.log("Next");
-            varPageNum = 1;
+            setPageNum((prevPageNum) => prevPageNum += 1)
         }
-        else if(event.target.textContent == "<<") {
+        else if (event.target.textContent == "<<") {
             console.log("Prev");
-            varPageNum = -1;
+            setPageNum((prevPageNum) => prevPageNum += 1)
         }
         else {
             console.log("Page" + event.target.textContent);
-            varPageNum = Number(event.target.textContent);
+            setPageNum((prevPageNum) => prevPageNum = Number(event.target.textContent))
         }
-        setPageNum((prevPageNum) => prevPageNum+=varPageNum)
-        console.log(pageNum);
     }
+
+    useEffect(() => {
+        // Change shown art pieces (shownArtPieces)
+        console.log(pageNum);
+    }, [pageNum])
 
     return (
         <div className={styles["art-gallery-container"]}>
@@ -43,7 +45,7 @@ export default function ArtGalleryPage() {
             <div className={styles["art-piece-list"]}>
                 {shownArtPieces.map((artItem: any) => <ArtPiece key={artItem.id} artPiece={artItem} />)}
             </div>
-            <Pagination handleSetPageNum={handleSetPageNum} pageNum={pageNum} maxPageNum={maxPageNum}/>
+            <Pagination handleSetPageNum={handleSetPageNum} pageNum={pageNum} maxPageNum={maxPageNum} />
         </div>
     )
 }
