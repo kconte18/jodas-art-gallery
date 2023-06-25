46ca5a3a3beacee693ed6ef46050a8946530962b
kconte18 <kconte810@gmail.com>
2023-06-25 18:43:52 -0500

    Nav bar vertical lines

--- src/components/header/navbar/NavBar.tsx
+++ src/components/header/navbar/NavBar.tsx
@@ -3,28 +3,21 @@ import styles from "./NavBar.module.css";
 import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
-    let homeActive = useRef(true);
-    let contactActive = useRef(false);
-    let aboutActive = useRef(false);
-
-    useEffect(() => {
-
-    },)
-
     return (
         <ul className={styles["navbar-container"]}>
+        <link href="https://fonts.googleapis.com/css2?family=Bacasime+Antique&display=swap" rel="stylesheet"></link>
             <li className={styles["navbar-item"]}>
                 <NavLink to="" className={({ isActive }) => isActive ? styles.active : ""}>
                     Gallery
                 </NavLink>
             </li>
-            <li className={styles["navbar-item"]}>|</li>
+            <li className={`${styles["navbar-item"]} ${styles["separator"]}`}>|</li>
             <li className={styles["navbar-item"]}>
                 <NavLink to="/contact-me" className={({ isActive }) => isActive ? styles.active : ""}>
                     Contact Me
                 </NavLink>
             </li>
-            <li className={styles["navbar-item"]}>|</li>
+            <li className={`${styles["navbar-item"]} ${styles["separator"]}`}>|</li>
             <li className={styles["navbar-item"]}>
                 <NavLink to="/about" className={({ isActive }) => isActive ? styles.active : ""}>
                     About
