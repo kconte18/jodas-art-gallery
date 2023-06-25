947b9c484d87b7ccf1e4fb64a5dc426952b302a2
kconte18 <kconte810@gmail.com>
2023-06-24 21:40:41 -0500

    Added responsivity to header

--- src/components/header/WebsiteTitle/WebsiteTitle.tsx
+++ src/components/header/WebsiteTitle/WebsiteTitle.tsx
@@ -3,6 +3,7 @@ import styles from "./WebsiteTitle.module.css";
 export default function WebsiteTitle() {
     return(
         <div className={styles["website-title-container"]}>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine" />
             <h1 className={styles["website-title"]}>Joda's Art Gallery</h1>
             <img className={styles["website-picture"]} src="https://www.si.edu/sites/default/files/blog/japanese-flying-squirrel-keishiro-shin_3-630x4201.jpg" alt="Oops"/>
         </div>
