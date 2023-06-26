f7526e1d442b6e66c04e8a1594cd24e51ddb0635
kconte18 <kconte810@gmail.com>
2023-06-25 20:26:32 -0500

    Files

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,5 @@
 import WebsiteTitle from "./WebsiteTitle/WebsiteTitle";
-import NavBar from "./NavBar/NavBar";
+import NavBar from "./Navbar/NavBar";
 import styles from "./Header.module.css";
 
 export default function Header(){
